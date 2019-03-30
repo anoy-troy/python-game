cacf6d109b795f2cc456b35541ec192a48c78713
Vinay252525 <35774542+Vinay252525@users.noreply.github.com>
2019-03-30 11:59:38 +0530

    Update First.c

--- First.c
+++ First.c
@@ -1,3 +1,4 @@
+/////////////////////////////// Question No. 1 /////////////////////////////////
 #include<stdio.h>
 
 int w_time(int q[][3],int n);
@@ -20,14 +21,14 @@ int main()
   for(int i=0;i<n;++i)
   {
 
-    printf("Enter Burst Time and Arrival Time for Process %d:",i+1 );
-//
+    printf("Enter Burst Time and Arrival Time for Process %d:", i+1);
+
     scanf("%d %d",&p[i][1],&p[i][2]);
   }
   printf("\n\n");
   int t=w_time(p,n);
 
-  printf("Total time taken by Processor for computation is:%d",t);
+  printf("\nTotal time taken by Processor for computation is:%d\n\n",t);
 
 }
 
@@ -44,7 +45,7 @@ int w_time(int q[][3],int n)
     }
 
 
-    int wt[50]={0},tat[50]={0},i=0,j=0,k=0,t,shortest=0,tot=0,ct[50]={0},min;
+    int wt[50]={0},tat[50]={0},i=0,j=0,k=0,t=0,shortest=0,tot=0,ct[50]={0},min;
 
     shortest=minimumBurstAndArrival(copyp,n);
     min=copyp[shortest][2];
@@ -78,20 +79,24 @@ int w_time(int q[][3],int n)
 
       if(t<=max)
       { j=0;
-         while(copyp[j][2]<=t)
-         {
-           if(copyp[shortest][1]>copyp[j][1])
-              shortest=j;
-           ++j;
-         }
+
+        for(int k=0;k<n;++k)
+        {
+          if(copyp[k][2]<=t)
+          {
+              if(copyp[shortest][1]>copyp[k][1])
+              shortest=k;
+
+          }
+        }
       }
     }
 
     for(i=0;i<n;++i)
     {
-      printf("%d\n",ct[i]);
-      wt[i]=ct[i]-q[i][1];
+      //printf("%d\n",ct[i]);
       tat[i]=ct[i]-q[i][2];
+      wt[i]=tat[i]-q[i][1];
     }
 
     showDetails(q,ct,wt,tat,n);
@@ -129,7 +134,7 @@ int minimumBurstAndArrival(int copyq[][3],int n)
 
     if(min==copyq[i][2])
     {
-      if(copyq[i][1]<copyq[sh][min])
+      if(copyq[i][1]<copyq[sh][1])
         sh=i;
     }
   }
@@ -144,13 +149,15 @@ void showDetails(int r[][3],int ct[],int wt[],int tat[],int n)
 
   for(int i=0;i<n;++i)
   {
-    printf("  %d          %d       %d        %d       %d       %d\n",r[i][0],r[i][1],r[i][2],ct[i],wt[i],tat[i]);
+    printf("  %d         %0.2d       %0.2d       %0.2d       %0.2d       %0.2d\n",r[i][0],r[i][1],r[i][2],ct[i],wt[i],tat[i]);
     sumwt=sumwt+wt[i];
     sumtat=sumtat+tat[i];
   }
 
+  float a = (float)sumwt/n;
+  float b = (float)sumtat/n;
 
-  printf("Average Waiting Time is: %d\n",sumwt/n);
-  printf("Average Turn Around Time is: %d \n",sumtat/n);
+  printf("\nAverage Waiting Time is: %0.2f\n",a);
+  printf("\nAverage Turn Around Time is: %0.2f \n",b);
 
 }
